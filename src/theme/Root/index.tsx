74696c3ac84d0bc0797b34c6ef6b6e644ad72632
Robbiekruszynski <robbie@apecoin.com>
2026-01-20 16:19:50 +0800

    adj on connect

--- src/theme/Root/index.tsx
+++ src/theme/Root/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import ChefAI from "../../components/ChefAI";
 
 export default function Root({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
-      <ChefAI />
     </>
   );
 }
